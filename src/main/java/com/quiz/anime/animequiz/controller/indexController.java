b357852baca22458824f4305645a019895871792
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-11-23 05:31:10 +0100

    deleted the entity classes and config because i want to restructure the app

--- src/main/java/com/quiz/anime/animequiz/controller/indexController.java
+++ src/main/java/com/quiz/anime/animequiz/controller/indexController.java
@@ -1,20 +1,11 @@
 package com.quiz.anime.animequiz.controller;
 
-import com.quiz.anime.animequiz.models.ParentResult;
-import com.quiz.anime.animequiz.service.ApiService;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class indexController {
-    private ApiService apiService;
-
-    public indexController(ApiService apiService) {
-        this.apiService = apiService;
-    }
 
     @RequestMapping("/")
     public String getIndexPage(Model model){
--- src/main/java/com/quiz/anime/animequiz/service/ApiService.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.anime.animequiz.service;
-
-import com.quiz.anime.animequiz.models.Result;
-
-import java.util.Set;
-
-public interface ApiService {
-    public Set<Result> getResult();
-}
